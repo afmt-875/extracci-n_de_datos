376f7a7ec818aa05bd1fe8562d904c325eb8e81c
afmt-875 <a01704728@tec.mx>
2024-04-08 20:00:25 -0600

    Nuevos Filtros

--- extraccion.py
+++ extraccion.py
@@ -16,7 +16,37 @@ print(filtro2)
 filtro3 = df.iloc[: , [1,3,4,10]]
 print(filtro3.head())
 
+#Filtro por filas con columnas
+df1= pd.read_excel('detalle_precios_productos_fabricados_2022.xlsx', index_col=1)
+filtro4=df1.loc[["2022-11-22","'2022-12-23"], ["SUBTOTAL_PARTIDA"]]
+print(filtro4.head())
+
+#Filtrar para que muestre los primeros n registros
+filtro5 = df.head(8)
+print(filtro5.head())
+
+#Filtrar por comparación
+filtro6 = df[df["SUBTOTAL_PARTIDA"] > 77000]
+print(filtro6.head())
+
+#Realizar una doble condición para filtrar
+filtro7 = df[(df["SUBTOTAL_PARTIDA"] > 77000) & (df["FECHA_DOC"] == "2022-05-24")]
+print(filtro7.head())
+
+#Realizar una condición u otra para filtrar
+filtro8 = df[(df["SUBTOTAL_PARTIDA"] > 77000)| (df["FECHA_DOC"] == "2022-05-24")]
+print(filtro8.head())
+
+#Filtro condición not
+filtro9 = df[~(df["SUBTOTAL_PARTIDA"] > 77000) & ~(df["FECHA_DOC"] == "2022-05-24")]
+print(filtro9.head())
+
 #Mandar a csv
 filtro1.to_csv("Entregable1.cvs")
 filtro2.to_csv("Entregable2.cvs")
 filtro3.to_csv("Entregable3.cvs")
+filtro5.to_csv("Entregable5.cvs")
+filtro6.to_csv("Entregable6.cvs")
+filtro7.to_csv("Entregable7.cvs")
+filtro8.to_csv("Entregable8.cvs")
+filtro9.to_csv("Entregable9.cvs")

b4d2cdc735107eeb48e765bc8d73e6781025bd73
afmt-875 <a01704728@tec.mx>
2024-04-10 18:50:06 -0600

    Final 04.10

--- practica_facturacion.py
+++ practica_facturacion.py
@@ -1,9 +1,10 @@
 import pandas as pd
+print("Inicio")
 df =pd.read_excel("datos_facturacion.xlsx")
 print(df.head())
 
-filtro1 = df[(df["CVE_CLPV"] >= 1000)] & (df["CVE_CLPV"] <= 2000)
-#print(filtro1)
+filtro1=df[(df["CVE_CLPV"] >= 1000) & (df["CVE_CLPV"]<=2000)]
+print(filtro1)
 filtro1.to_csv("practica_facturacion_1.csv")
 
 filtro2 = df[~(df["CVE_VEND"] == 5.0 ) & ~(df["CVE_VEND"] == 4.0)]
@@ -11,7 +12,7 @@ print(filtro2)
 
 filtro3=df[df["FECHA_ENT"] == '2022-28-2']
 print(filtro3)
-     
+
 filtro4=df[(df["CAN_TOT"] < 5951.7)| (df["STATUS"] == "E")]
 print(filtro4)
 
@@ -21,7 +22,7 @@ print(filtro5)
 filtro6 = df.iloc[7001:7099, :  ] 
 print(filtro6)
 
-df1= pd.read_excel('Datos de Facturación.xlsx', index_col=3)
+df1= pd.read_excel('datos_facturacion.xlsx', index_col=3)
 df1.head()
 
 filtro7=df1.loc[[1.0,2.0], ["FECHAELAB"]]

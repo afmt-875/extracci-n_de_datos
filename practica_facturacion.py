ce279c1502f5f4124db0c53adfa3109c4e5b33cb
afmt-875 <a01704728@tec.mx>
2024-04-10 18:35:24 -0600

    Ultimos Cambios

--- practica_facturacion.py
+++ practica_facturacion.py
@@ -3,5 +3,35 @@ df =pd.read_excel("datos_facturacion.xlsx")
 print(df.head())
 
 filtro1 = df[(df["CVE_CLPV"] >= 1000)] & (df["CVE_CLPV"] <= 2000)
-print(filtro1)
+#print(filtro1)
 filtro1.to_csv("practica_facturacion_1.csv")
+
+filtro2 = df[~(df["CVE_VEND"] == 5.0 ) & ~(df["CVE_VEND"] == 4.0)]
+print(filtro2)
+
+filtro3=df[df["FECHA_ENT"] == '2022-28-2']
+print(filtro3)
+     
+filtro4=df[(df["CAN_TOT"] < 5951.7)| (df["STATUS"] == "E")]
+print(filtro4)
+
+filtro5 = df.iloc[ : , [0, 6, 7,9]]  
+print(filtro5)
+
+filtro6 = df.iloc[7001:7099, :  ] 
+print(filtro6)
+
+df1= pd.read_excel('Datos de Facturación.xlsx', index_col=3)
+df1.head()
+
+filtro7=df1.loc[[1.0,2.0], ["FECHAELAB"]]
+print(filtro7)
+
+
+filtro1.to_csv("Entregable1.1.csv")
+filtro2.to_csv("Entregable2.1.csv")
+filtro3.to_csv("Entregable3.1.csv")
+filtro4.to_csv("Entregable4.1.csv")
+filtro5.to_csv("Entregable5.1.csv")
+filtro6.to_csv("Entregable6.1.csv")
+filtro7.to_csv("Entregable7.1.csv")
